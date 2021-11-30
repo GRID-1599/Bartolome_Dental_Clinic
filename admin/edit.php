fcf2a1048f0271da817c48f63ff93a7efd6d5ead
Jude <catudiochristianjude@gmail.com>
2021-12-01 02:09:57 +0800

    admin edit modified

--- admin/edit.php
+++ admin/edit.php
@@ -28,53 +28,103 @@
 
                     <h1>Admin Edit Account</h1>
 
-                    <div class="container-sm ">
-                        <div class="col-xxl-8 justify-content-start">
-                            <div class="row mb-3">
-                                <div class="col-md-6">
+                    <div class="container">
+                        <div class="row">
+                            <p>Account information</p>
+                            <div class="col-lg-6 justify-content-start px-3 mb-5">
+                                <!-- first name  -->
+                                <div class="col mb-3">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputAdminFirstName" type="text" placeholder="Enter your first name" />
                                         <label for="inputAdminFirstName">First name</label>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <!-- last name  -->
+                                <div class="col mb-3">
                                     <div class="form-floating">
                                         <input class="form-control" id="inputAdminLastName" type="text" placeholder="Enter your last name" />
                                         <label for="inputAdminLastName">Last name</label>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-6">
+                                <!-- email  -->
+                                <div class="col">
                                     <div class="form-floating mb-3">
                                         <input class="form-control" id="inputAdminEmail" type="email" placeholder="name@example.com" />
                                         <label for="inputAdminEmail">Email address</label>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <!-- contact  -->
+                                <div class="col">
                                     <div class="form-floating mb-3">
                                         <input class="form-control" id="inputAdminContact" type="text" placeholder="+63**********" />
                                         <label for="inputAdminContact">Contact Number</label>
                                     </div>
                                 </div>
+
+
+
+                                <div class="mt-4 mb-0">
+                                    <div class="d-grid"><button class="btn bg-pink text-white btn-block" id="btnAdminCreate">Save Changes</button></div>
+                                </div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputAdminPassword" type="password" placeholder="Create a password" />
-                                        <label for="inputAdminPassword">Password</label>
+                            <div class="col">
+                                <div class="container ">
+                                    <div class="row shadow mb-5 rounded">
+                                        <p class="my-3 h5">For Changing Admin Username</p>
+                                        <div class="mb-3">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputAdminFirstName" type="text" placeholder="Enter your first name" />
+                                                <label for="inputAdminFirstName">New User Name</label>
+                                            </div>
+
+                                            <div class="form-floating ">
+                                                <input class="form-control is-invalid" id="inputAdminPasswordConfirm" type="password" placeholder="Confirm password" />
+                                                <label for="inputAdminPasswordConfirm">Input Password</label>
+
+                                                <div class="invalid-feedback">
+                                                    Incorrect Password
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="mt-4 mb-5">
+                                            <button class="btn bg-pink text-white w-auto float-end" id="btnAdminCreate">Save Username Changes</button>
+
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputAdminPasswordConfirm" type="password" placeholder="Confirm password" />
-                                        <label for="inputAdminPasswordConfirm">Confirm Password</label>
+
+                                    <div class="row shadow mb-5 rounded">
+                                        <p class="my-3 h5">Changing Password</p>
+                                        <div class="mb-3 ">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputAdminPassword" type="password" placeholder="Current password" />
+                                                <label for="inputAdminPassword">Current Password</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputAdminPassword" type="password" placeholder="Create a new password" />
+                                                <label for="inputAdminPassword">New Password</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control is-invalid" id="inputAdminPasswordConfirm" type="password" placeholder="Confirm password" />
+                                                <label for="inputAdminPasswordConfirm">Confirm New Password</label>
+                                                <div class="valid-feedback">
+                                                    Looks good!
+                                                </div>
+                                                <div class="invalid-feedback">
+                                                    Password not match
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="mt-4 mb-5">
+                                            <button class="btn bg-pink text-white w-auto float-end" id="btnAdminCreate">Save Username</button>
+
+                                        </div>
                                     </div>
+
                                 </div>
-                            </div>
-                            <div class="mt-4 mb-0">
-                                <div class="d-grid"><button class="btn bg-pink text-white btn-block" id="btnAdminCreate">Save Changes</button></div>
                             </div>
                         </div>
                     </div>
