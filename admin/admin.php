cd8c111e900adc63abb07502d2a818f215988f15
Jude <catudiochristianjude@gmail.com>
2021-11-30 18:07:20 +0800

    admin change

--- admin/admin.php
+++ admin/admin.php
@@ -20,21 +20,57 @@
                     <ol class="breadcrumb mb-4">
                         <li class="breadcrumb-item active">List of Admin</li>
                     </ol>
-                    
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <i class="fas fa-table me-1"></i> Admins
+
+                    <div class="container-fluid border">
+                        <div class="row mb-3  ">
+                            <a href="service/add">
+                                <button type="button" class="btn btn-info float-end"> <i class="fas fa-plus " id="btnAddNewService"></i> Register New Admin</button>
+                            </a>
                         </div>
-                        <div class="card-body">
-                            <?php include 'adminTable.php' ?>
+                        <div class="row g-3 border">
+                                <div class="card me-3" style="width: 18rem;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">jude</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Admin</h6>
+                                        <p class="card-text">
+                                            Christian jude catudior
+                                        <p class="text-end">
+                                            asdadad<br>
+                                            asdsadasd@asdasd
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="card me-3" style="width: 18rem;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">jude</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Admin</h6>
+                                        <p class="card-text">
+                                            Christian jude catudior
+                                        <p class="text-end">
+                                            asdadad<br>
+                                            asdsadasd@asdasd
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="card me-3" style="width: 18rem;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">jude</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Admin</h6>
+                                        <p class="card-text">
+                                            Christian jude catudior
+                                        <p class="text-end">
+                                            asdadad<br>
+                                            asdsadasd@asdasd
+                                        </p>
+                                    </div>
+                                </div>
                         </div>
                     </div>
-                </div>
             </main>
             <?php include 'html-footer.php' ?>
         </div>
     </div>
-    <?php include'scripts.php'?>
+    <?php include 'scripts.php' ?>
 </body>
 
 </html>
