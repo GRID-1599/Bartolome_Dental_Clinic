468b934eba5a32d089a635314948aee11cf734fc
Jude <catudiochristianjude@gmail.com>
2021-12-02 17:13:43 +0800

    try

--- test.php
+++ test.php
@@ -1,3 +1,30 @@
 <!-- this only for trying -->
 <?php
+//  password hashing  
+$br = "<br><br>";
+$str = "catudio";
+$str1 = "catudio1";
+echo $str;
+echo $br;
 
+$password =  password_hash($str, PASSWORD_DEFAULT);
+
+echo $password;
+
+echo $br;
+
+echo strlen( $password);
+
+echo $br; 
+
+// echo 'return'.password_verify($str, $password);
+echo 'return: '.password_verify($str, "password");
+
+echo $br; 
+
+if(password_verify($str1, $password)){
+    echo "true";
+}else{
+    echo "false";
+
+}
