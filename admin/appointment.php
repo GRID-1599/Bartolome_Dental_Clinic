e6152cf97ed7ec26be3b96de2d9b14ef7df88785
Jude <catudiochristianjude@gmail.com>
2021-12-05 16:57:01 +0800

    appointment filter added

--- admin/appointment.php
+++ admin/appointment.php
@@ -1,7 +1,7 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 
-    
+
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +24,63 @@ session_start();
                     <ol class="breadcrumb mb-4">
                         <li class="breadcrumb-item active" aria-current="page">List of Appoinment</li>
                     </ol>
+
+
+
+
+                    <div class="row mb-3 py-3 border">
+                        <div class="col-5 border">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <div class="dropdown dropend">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="ddFilter" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Filter by :
+                                        </button>
+                                        <ul class="dropdown-menu " aria-labelledby="ddFilter">
+                                            <li class=" ps-3" id="filterID">Patient Id</li>
+                                            <li class=" ps-3" id="filterAppDate">Appointment Date</li>
+                                            <li class=" ps-3" id="filterDateCreate">Date Created</li>
+                                            <li class=" ps-3" id="filterAmount">Amount</li>
+                                            <li class=" ps-3" id="filterPaid">Paid</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <span id="filteredBy"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-7 border">
+                            <div class="row filterInputs unShow" id="inptPatientID">
+                                <label for="appPatientId" class="col-sm-3 col-form-label">Patient Id </label>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" id="appPatientId">
+                                </div>
+                            </div>
+                            <div class="row filterInputs unShow " id="inptAppDate">
+                                <div class="col">
+                                    <div class="row">
+                                        <label for="appDateStart" class="col-sm-1 col-form-label ">Start</label>
+                                        <div class="col-sm-5">
+                                            <input type="date" class="form-control" id="appDateStart">
+                                        </div>
+                                        <label for="appDateEnd" class="col-sm-1 col-form-label">End</label>
+                                        <div class="col-sm-5">
+                                            <input type="date" class="form-control" id="appDateEnd">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row filterInputs " id="inptDateCreated">
+                                <label for="appDateCreated" class="col-sm-3 col-form-label ">Date Created </label>
+                                <div class="col-sm-7">
+                                    <input type="date" class="form-control" id="appDateCreated">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
 
                     <div class="card mb-4">
                         <div class="card-header">
--- admin/js/appointment.js
+++ admin/js/appointment.js
@@ -20,4 +20,46 @@ $(document).ready(function() {
         appoinmentEventUpdate();
 
     }, 1000);
+
+
+    // $('#filterDD').on('hide.bs.dropdown', function() {
+    //     console.log(1);
+    //     return false;
+    // });
+
+    $('#filterID').click(function() {
+        $('#filteredBy').text("Patient Id")
+        unShowAllFilterInpt()
+        $('#inptPatientID').removeClass("unShow")
+    });
+
+    $('#filterAppDate').click(function() {
+        $('#filteredBy').text("Appoinment Date")
+        unShowAllFilterInpt()
+        $('#inptAppDate').removeClass("unShow")
+
+    });
+
+    $('#filterDateCreate').click(function() {
+        $('#filteredBy').text("Date Created")
+        unShowAllFilterInpt()
+    });
+
+    $('#filterAmount').click(function() {
+        $('#filteredBy').text("Amount")
+        unShowAllFilterInpt()
+    });
+
+    $('#filterPaid').click(function() {
+        $('#filteredBy').text("Paid")
+        unShowAllFilterInpt()
+    });
+
 });
+
+function unShowAllFilterInpt() {
+    $('.filterInputs').each(function() {
+        $(this).addClass("unShow");
+    });
+
+}
